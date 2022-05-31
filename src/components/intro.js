27f6e42f9ab98f96a4b8624b874a5439cee66eac
StevieP <stevenpreston2014@gmail.com>
2022-05-31 13:27:05 +0100

    Added intro content, and fixed intro.css with corresponding class names

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "../stylesheets/intro.css";
 
-class footer extends React.Component{
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
 
-}
+class intro extends React.Component{
+     
+      render() {
+        return (
+          <div id="intro">
+              <span className="intro-title">
+                {"Hello!, I'm "}
+                <span className="intro-name">{"Stevie"}</span>
+                {"."}
+              </span>
+              <div className="intro-subtitle">I like to learn through programming.</div>
+              <div className="intro-desc">
+                I'm a software engineer based in Belfast. I have
+                interest in deep learning, web applications,
+                cyber security, and everything in between.
+              </div>
+              <a
+                href="mailto:stevenpreston2014@gmail.com"
+                className="intro-contact"
+              >
+                <EmailRoundedIcon></EmailRoundedIcon>
+                {"  " + "Say hi!"}
+              </a>
+          </div>
+        );
+      }
+    }
+
 
 export default intro;
