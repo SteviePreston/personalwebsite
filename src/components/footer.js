289a8f727b9e40152a3fcfd4a8b0408cb4ac352f
StevieP <stevenpreston2014@gmail.com>
2022-05-31 12:48:16 +0100

    Added material ui Libary, added content to the footer & navbar component templates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "../stylesheets/footer.css";
 
-class footer extends React.Component{
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-}
+class footer extends React.Component{
+  
+      render() {
+        return (
+            <div className="footer-text">
+                <p>
+                    Made & designed by <a href="mailto:stevenpreston2014@gmail.com"><span className="footer-name">Stevie</span></a> with ♥
+                </p>
+                <div className="footer-logos">
+                <a href="mailto:stevenpreston2014@gmail.com">
+                    <EmailRoundedIcon style={{ fontSize: 20 }}></EmailRoundedIcon>
+                </a>
+                <a href="https://github.com/SteviePreston">
+                    <GitHubIcon style={{ fontSize: 19 }}></GitHubIcon>
+                </a>
+                <a href="https://www.linkedin.com/in/steviep-ai/">
+                    <LinkedInIcon style={{ fontSize: 21 }}></LinkedInIcon>
+                </a>
+                </div>
+            </div>
+        );
+      }
+    }
 
 export default footer;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,22 @@ import React from "react";
 import "../stylesheets/navbar.css";
 
 class navbar extends React.Component{
+    render(){
+        const links = [
+            <a href="#intro">/intro</a>,
+            <a href="#aboutme">/about-me</a>,
+            <a href="#jobexperience">/jobexperience</a>,
+            <a href="#projects">/projects</a>
+          ];
 
+        return (
+            <div className="navbar-links">
+                {links.map((link, i) => (
+                    <div>{link}</div>
+                ))}
+              </div>
+        );
+    }
 }
 
 export default navbar;
