6c05e7fe8063a361d6c5ed9c7937df52314cacd2
StevieP <stevenpreston2014@gmail.com>
2022-06-04 16:39:27 +0100

    Added content to the project component

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,48 @@ import React from "react";
 import "../stylesheets/Projects.css";
 
 class Projects extends React.Component{
-    render(){
+    ProjectList = () => {
+        const classes = useStyles();
+        const [value, setValue] = React.useState(0);
+      
+        const projectItems = {
+          "BT": {
+            projectSubTitle: "Software Engineering Placement",
+            techStack: "Python",
+            desc: [
+              "Developed within the BT secure payments team under BT's subteam 'ETC.' as part of the Digital space.",
+              "Worked in a plethora of areas within the project: Frontend (thistle), Backend (Buynet) & CLI Tools (Batch Payment Interface) ",
+              "Relevant technologies/tools used: WLS, SuperPutty, Postman, Citrix, SQL Plus, Oracle SQL Developer, Git, VI & RHEL VM."
+            ]
+          },
+          "BT": {
+            projectSubTitle: "Software Engineering Placement",
+            techStack: "Python",
+            desc: [
+              "Developed within the BT secure payments team under BT's subteam 'ETC.' as part of the Digital space.",
+              "Worked in a plethora of areas within the project: Frontend (thistle), Backend (Buynet) & CLI Tools (Batch Payment Interface) ",
+              "Relevant technologies/tools used: WLS, SuperPutty, Postman, Citrix, SQL Plus, Oracle SQL Developer, Git, VI & RHEL VM."
+            ]
+          },
+          "BT": {
+            projectSubTitle: "Software Engineering Placement",
+            techStack: "Python",
+            desc: [
+              "Developed within the BT secure payments team under BT's subteam 'ETC.' as part of the Digital space.",
+              "Worked in a plethora of areas within the project: Frontend (thistle), Backend (Buynet) & CLI Tools (Batch Payment Interface) ",
+              "Relevant technologies/tools used: WLS, SuperPutty, Postman, Citrix, SQL Plus, Oracle SQL Developer, Git, VI & RHEL VM."
+            ]
+          },
+          "BT": {
+            projectSubTitle: "Software Engineering Placement",
+            techStack: "Python",
+            desc: [
+              "Developed within the BT secure payments team under BT's subteam 'ETC.' as part of the Digital space.",
+              "Worked in a plethora of areas within the project: Frontend (thistle), Backend (Buynet) & CLI Tools (Batch Payment Interface) ",
+              "Relevant technologies/tools used: WLS, SuperPutty, Postman, Citrix, SQL Plus, Oracle SQL Developer, Git, VI & RHEL VM."
+            ]
+          }
+        };
         return (
             <div className="projects">
 
